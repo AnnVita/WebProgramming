1079683be2756c3ae88f3dfe506c3dbe98c10501
AnnVita <kublitskayavd@yandex.ru>
2017-02-15 11:09:39 +0000

    начат canvas

---
+++ shapes/script.js
@@ -0,0 +1,37 @@
+/**
+ * Created by annvita on 15.02.17.
+ */
+function start()
+{
+    var canvas = document.getElementById("canvas");
+    context = canvas.getContext('2d');
+
+    var shape = class {
+        _fillColor : "#00001F",
+        _borderColor : "#42E818",
+
+        set SetBorderColor(newValue)
+        {
+            this._borderColor = newValue;
+        }
+        get GetBorderColor()
+        {
+            return this._fillColor();
+        }
+
+        set SetFillColor(newValue)
+        {
+            this._fillColor = newValue;
+        }
+        get GetFillColor()
+        {
+            return this._fillColor();
+        }
+
+        draw(target);
+        calculateArea();
+        calculatePerimeter();
+    };
+    context.fillStyle = "#00001F";
+    context.fillRect(0, 20, 30, 30);
+}
