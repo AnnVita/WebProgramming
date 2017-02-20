9d4ba5e1c04e4ca006faa537f0bdaef34ac1186e
AnnVita <kublitskayavd@yandex.ru>
2017-02-20 23:07:56 +0000

    Классы Shape и Circle

--- shapes/script.js
+++ shapes/script.js
@@ -4,34 +4,65 @@
 function start()
 {
     var canvas = document.getElementById("canvas");
-    context = canvas.getContext('2d');
+    var context = canvas.getContext('2d');
 
-    var shape = class {
-        constructor(){
-            _fillColor = "#00001F";
-            _borderColor = "#42E818";
-        }
-        set SetBorderColor(newValue)
-        {
-            this._borderColor = newValue;
-        }
-        get GetBorderColor()
+    var point = {
+        x : 0,
+        y : 0
+    };
+
+    var shape = {
+        _fillColor : "#00001F",
+        _borderColor : "#FFF000",
+        _borderWidth : 2,
+        get fillColor()
         {
             return this._fillColor;
-        }
-
-        set SetFillColor(newValue)
+        },
+        set fillColor(newValue)
         {
             this._fillColor = newValue;
-        }
-        get GetFillColor()
+        },
+        get borderColor()
         {
-            return this._fillColor;
+            return this._borderColor;
+        },
+        set fillColor(newValue)
+        {
+            this._borderColor = newValue;
+        },
+        draw : function(target) {},
+        calculateArea : function() {},
+        calculatePerimeter : function() {}
+    };
+
+    var circle = {
+        __proto__ : shape,
+        _center : point,
+        _radius : 20,
+
+        draw: function (target) {
+            target.beginPath();
+            target.fillStyle = this._fillColor;
+            target.strokeStyle = this._borderColor;
+            target.arc(this._center.x, this._center.y, this._radius, 0, Math.PI*2, false);
+            target.closePath();
+            target.fill();
+            target.stroke();
+        },
+
+        calculateArea : function()
+        {
+            return this._radius * this._radius * Math.PI;
+        },
+
+        calculatePerimeter : function()
+        {
+            return 2 * this._radius * Math.PI;
         }
-        draw(target){}
-        calculateArea(){}
-        calculatePerimeter(){}
-    }
+    };
+
+    circle.draw(context);
     context.fillStyle = "#00001F";
     context.fillRect(0, 20, 30, 30);
 }
